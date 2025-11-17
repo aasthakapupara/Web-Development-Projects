83461840179e5a33529c78e207de856e01df3f14
Aastha Kapupara <aasthakapupara15@gmail.com>
2025-11-17 16:33:04 +0530

    Update footer.php
    Removed the developer attribution from the footer.

--- Hostel-Management-System/includes/footer.php
+++ Hostel-Management-System/includes/footer.php
@@ -1,3 +1,4 @@
 <footer class="footer text-center text-muted">
-&copy; <?php echo date("Y"); ?> - Hostel Management System - Developed by <a href="https://codeastro.com">CodeAstro</a>
+&copy; <?php echo date("Y"); ?> - Hostel Management System
+
 </footer>
